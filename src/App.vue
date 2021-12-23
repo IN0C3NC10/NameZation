ed6116abbe03861a2085d88d9ca6180e2e5e8cdb
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-12-23 19:39:49 -0300

    Sufix and Prefix list replaceds by Item Component

--- src/App.vue
+++ src/App.vue
@@ -9,65 +9,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-md">
-                    <h5>
-                        Prefixos
-                        <span class="badge back-main">{{ prefixes.length }}</span>
-                    </h5>
-                    <div class="card">
-                        <div class="card-body">
-                            <ul class="list-group">
-                                <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                                    <div class="row">
-                                        <div class="col-md">
-                                            {{ prefix }}
-                                        </div>
-                                        <div class="col-md text-right">
-                                            <button v-on:click="deletePrefix(prefix)" class="btn btn-danger"><span class="fa fa-trash"></span></button>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                            <br />
-                            <div class="input-group">
-                                <input v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" class="form-control" type="text" placeholder="Digite o prefixo" />
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" v-on:click="addPrefix(prefix)">
-                                        <span class="fa fa-plus"></span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <AppItemsList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemsList>
                 </div>
                 <div class="col-md">
-                    <h5>
-                        Sufixos <span class="badge back-main">{{ sufixes.length }}</span>
-                    </h5>
-                    <div class="card">
-                        <div class="card-body">
-                            <ul class="list-group">
-                                <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                                    <div class="row">
-                                        <div class="col-md">
-                                            {{ sufix }}
-                                        </div>
-                                        <div class="col-md text-right">
-                                            <button v-on:click="deleteSufix(sufix)" class="btn btn-danger"><span class="fa fa-trash"></span></button>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                            <br />
-                            <div class="input-group">
-                                <input v-model="sufix" v-on:keyup.enter="addSufix(sufix)" class="form-control" type="text" placeholder="Digite o sufixo" />
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" v-on:click="addSufix(sufix)">
-                                        <span class="fa fa-plus"></span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <AppItemsList title="Sufixos" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemsList>
                 </div>
             </div>
             <br />
@@ -100,12 +45,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import AppItemsList from './components/AppItemsList.vue';
 export default {
+  components: { AppItemsList },
     name: "App",
     data() {
         return {
-            prefix: "",
-            sufix: "",
             prefixes: ["Pre","Su","Name"],
             sufixes: ["Fix", "Zation"],
         };
@@ -114,11 +59,9 @@ export default {
     methods: {
         addPrefix(prefix) {
             this.prefixes.push(prefix);
-            this.prefix = "";
         },
         addSufix(sufix) {
             this.sufixes.push(sufix);
-            this.sufix = "";
         },
         deletePrefix(prefix) {
             this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
---
+++ src/components/AppItemsList.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+    <h5>
+        {{ title }}
+        <span class="badge back-main">{{ items.length }}</span>
+    </h5>
+    <div class="card">
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item" v-for="item in items" v-bind:key="item">
+                    <div class="row">
+                        <div class="col-md">
+                            {{ item }}
+                        </div>
+                        <div class="col-md text-right">
+                            <button v-on:click="deleteItem(item)" class="btn btn-danger"><span class="fa fa-trash"></span></button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <br />
+            <div class="input-group">
+                <input v-model="item" v-on:keyup.enter="addItem(item)" class="form-control" type="text" placeholder="Digite o item" />
+                <div class="input-group-append">
+                    <button class="btn btn-primary" v-on:click="addItem(item)">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'AppItemsList',
+    props: ['title','items'],
+    data() {
+        return {
+            item: ""
+        };
+    },
+
+    methods: {
+        addItem(item) {
+            // ..recupera o método passado no componente
+            this.$emit("addItem", item);
+            this.item = "";
+        },
+        deleteItem(item) {
+            this.$emit("deleteItem", item);
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
