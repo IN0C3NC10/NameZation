bf58b2194b15320b8b074302015f13e9bad99eac
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-12-22 23:20:32 -0300

    Vue-Format applied and plus-button added

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,107 @@
 <template>
-  <div>
+<div>
     <div id="slogan" class="text-center">
-      <h1>NameZation</h1>
-      <br />
-      <h6 class="text-secondary">Seu incrível gerador de nomes!</h6>
+        <h1>NameZation</h1>
+        <br />
+        <h6 class="text-secondary">Seu incrível gerador de nomes!</h6>
     </div>
     <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <h5>
+                        Prefixos
+                        <span class="badge back-main">{{ prefixes.length }}</span>
+                    </h5>
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                                    {{ prefix }}
+                                </li>
+                            </ul>
+                            <br />
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" v-on:click="addPrefix()">
+                                        <span class="fa fa-plus"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <h5>
+                        Sufixos <span class="badge back-main">{{ sufixes.length }}</span>
+                    </h5>
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                                    {{ sufix }}
+                                </li>
+                            </ul>
+                            <br />
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" v-on:click="addSufix()">
+                                        <span class="fa fa-plus"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br />
             <h5>
-              Prefixos
-              <span class="badge back-main">{{ prefixes.length }}</span>
+                Domínios <span class="badge back-main">{{ domains.length }}</span>
             </h5>
             <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="prefix in prefixes"
-                    v-bind:key="prefix"
-                  >
-                    {{ prefix }}
-                  </li>
-                </ul>
-                <br />
-                <input
-                  class="form-control"
-                  type="text"
-                  placeholder="Digite o prefixo"
-                />
-              </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+                            {{ domain }}
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </div>
-          <div class="col-md">
-            <h5>
-              Sufixos <span class="badge back-main">{{ sufixes.length }}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="sufix in sufixes"
-                    v-bind:key="sufix"
-                  >
-                    {{ sufix }}
-                  </li>
-                </ul>
-                <br />
-                <input
-                  class="form-control"
-                  type="text"
-                  placeholder="Digite o sufixo"
-                />
-              </div>
-            </div>
-          </div>
         </div>
-        <br/>
-        <h5>Domínios <span class="badge back-main">{{ domains.length }}</span></h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
-                {{ domain }}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 export default {
-  name: "App",
-  data() {
-    return {
-      prefixes: ["Air", "Jet", "Flight"],
-      sufixes: ["Hub", "Station", "Mart"],
-      domains:["AirHub","AirStation"],
-    };
-  },
+    name: "App",
+    data() {
+        return {
+            prefixes: ["Air", "Jet", "Flight"],
+            sufixes: ["Hub", "Station", "Mart"],
+            domains: ["AirHub", "AirStation"],
+        };
+    },
 };
 </script>
 
 <style>
 #slogan {
-  margin-top: 30px;
-  margin-bottom: 30px;
+    margin-top: 30px;
+    margin-bottom: 30px;
 }
 
 #main {
-  background-color: #f1f1f1;
-  padding-top: 30px;
-  padding-bottom: 30px;
+    background-color: #f1f1f1;
+    padding-top: 30px;
+    padding-bottom: 30px;
 }
 
 .back-main {
-  background-color: rgb(8, 90, 167);
+    background-color: rgb(8, 90, 167);
 }
 </style>
